9d2797c79125f39e7604b30f0afa35b766503d7c
guiags <guilhermeaugustogs2999@gmail.com>
2025-12-30 19:25:59 -0300

    commit 30122925 1850

--- resources/views/certificado/gerar_certificado.blade.php
+++ resources/views/certificado/gerar_certificado.blade.php
@@ -11,7 +11,7 @@
     </style>
 </head>
 <body>
-    <form action="gerar_pdf.php" method="POST">
+    <form action="/gerar_pdf.php" method="POST">
         <input type="text" name="nome" placeholder="Nome do Aluno" required>
         <input type="text" name="data" placeholder="Data Divinópolis-MG, 24 de Dezembro de 2025" required>
         <button type="submit">Gerar PDF</button>
